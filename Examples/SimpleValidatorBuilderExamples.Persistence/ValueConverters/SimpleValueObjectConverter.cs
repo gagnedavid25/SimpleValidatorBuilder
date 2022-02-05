ce48c49d04b99d5d69c3cd4397c42d3423d07928
David Gagné <dgagne25@hotmail.com>
2022-02-05 12:59:06 -0500

    Reflection optimization

--- Examples/SimpleValidatorBuilderExamples.Persistence/ValueConverters/SimpleValueObjectConverter.cs
+++ Examples/SimpleValidatorBuilderExamples.Persistence/ValueConverters/SimpleValueObjectConverter.cs
@@ -8,19 +8,19 @@ namespace SimpleValidatorBuilderExamples.Persistence.ValueConverters;
 internal class SimpleValueObjectConverter<TSimpleValueObject, TValue> : ValueConverter<TSimpleValueObject, TValue>
     where TSimpleValueObject : SimpleValueObject<TValue>
 {
+    private static readonly Func<TValue, Result<TSimpleValueObject, Error>> FactoryMethodDelegate =
+        (Func<TValue, Result<TSimpleValueObject, Error>>)Delegate.CreateDelegate(
+            typeof(Func<TValue, Result<TSimpleValueObject, Error>>), 
+            SimpleValueObjectConverter.GetStaticFactoryMethod(typeof(TSimpleValueObject)));
+
     public SimpleValueObjectConverter()
         : base(vo => vo.Value, value => SimpleValueObjectFactory(value))
     {
     }
 
-    public static TSimpleValueObject SimpleValueObjectFactory(TValue value)
+    private static TSimpleValueObject SimpleValueObjectFactory(TValue value)
     {
-        var factoryMethodInfo = SimpleValueObjectConverter.GetStaticFactoryMethod(typeof(TSimpleValueObject));
-
-        if (factoryMethodInfo is null)
-            SimpleValueObjectConverter.ThrowStaticFactoryMethodMissing(typeof(TSimpleValueObject).Name);
-
-        var result = (Result<TSimpleValueObject, Error>)factoryMethodInfo!.Invoke(null, new object[] { value! })!;
+        var result = FactoryMethodDelegate(value!);
         return result.Value;
     }
 }
@@ -29,17 +29,24 @@ internal static class SimpleValueObjectConverter
 {
     public const string StaticFactoryMethodName = "Create";
 
-    public static readonly Type TypeToSearch = typeof(SimpleValueObject<>);
+    public static readonly Type SimpleValueObjectType = typeof(SimpleValueObject<>);
     public static readonly Type ConverterType = typeof(SimpleValueObjectConverter<,>);
 
     public static bool IsSimpleValueObjectType(this Type type)
         => type.IsPublic && type.IsClass && !type.IsNested &&
             (type.BaseType?.IsGenericType ?? false) &&
-            type.BaseType.GetGenericTypeDefinition() == TypeToSearch;
+            type.BaseType.GetGenericTypeDefinition() == SimpleValueObjectType;
 
-    public static MethodInfo? GetStaticFactoryMethod(Type simpleValueObjectType)
-        => simpleValueObjectType.GetMethod(StaticFactoryMethodName, BindingFlags.Public | BindingFlags.Static);
+    public static MethodInfo GetStaticFactoryMethod(Type simpleValueObjectType)
+    {
+        var factoryMethod = simpleValueObjectType.GetMethod(StaticFactoryMethodName, BindingFlags.Public | BindingFlags.Static);
+
+        if (factoryMethod is null)
+            ThrowStaticFactoryMethodMissing(simpleValueObjectType.Name);
+        
+        return factoryMethod!;
+    }
 
     public static void ThrowStaticFactoryMethodMissing(string simpleValueObjectName)
-        => throw new InvalidOperationException($"The value object '{simpleValueObjectName}' must have the static factory method '{StaticFactoryMethodName}'.");
+        => throw new InvalidOperationException($"The value object '{simpleValueObjectName}' must have the static factory method 'Result<TSimpleValueObject, Error> {StaticFactoryMethodName}(TValue value)'.");
 }
